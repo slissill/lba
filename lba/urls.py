9d55b2b56358f0cc9484e3c2e6bb39152891a517
slissill <slissill@gmail.com>
2025-03-29 22:43:08 +0100

    index

--- lba/urls.py
+++ lba/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.hello_world, name='home'),
+    path('', views.home, name='home'),
     path('helloworld', views.helloworld, name='helloworld'),
 ]
--- lba/views.py
+++ lba/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def hello_world(request):    
-    return HttpResponse("Hello World")
+def test(request):    
+    return HttpResponse("test Hello World")
 
 def helloworld(request):        
     return render(request, "lba/helloworld.html")
+
+def home(request):        
+    return render(request, "lba/home.html")
